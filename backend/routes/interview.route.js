640be559e7891bda19a91feae085e57617be5822
Sumit Gupta <cricsumit24@gmail.com>
2026-03-23 22:55:34 +0530

    implement route for interview

--- backend/routes/interview.route.js
+++ backend/routes/interview.route.js
@@ -1,11 +1,13 @@
 import express from "express"
 import verifyJWT from "../middlewares/auth.middleware.js"
 import { upload } from "../middlewares/multer.middleware.js"
-import { analyzeResume, generateQuestions } from "../controllers/interview.controller.js"
+import { analyzeResume, finishInterview, generateQuestions, submitAnswer } from "../controllers/interview.controller.js"
 
 const interviewRouter = express.Router()
 
 interviewRouter.post("/resume", verifyJWT, upload.single("resume"), analyzeResume)
 interviewRouter.post("/generate-questions", verifyJWT, generateQuestions)
+interviewRouter.post("/submit-answer", verifyJWT, submitAnswer)
+interviewRouter.post("/finish", verifyJWT, finishInterview)
 
 export default interviewRouter
